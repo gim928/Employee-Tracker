4fe7b574c4fd5f80746b24593383c142c9049789
Gina Im <eaim928@gmail.com>
2021-09-01 08:25:37 -0600

    added more promts and show table functions

--- index.js
+++ index.js
@@ -1,49 +1,144 @@
-// const inquirer = require("inquirer");
-// const sequelize = require("../config/connection");
+const inquirer = require("inquirer");
+const sequelize = require("./config/connection");
+const table = require("console.table");
 
-// async function startProgram() {
-//   const answers = await inquirer.prompt([
-//     {
-//       type: "list",
-//       message: "What would you like to do?",
-//       choices: [
-//         "View all departments",
-//         "View all roles",
-//         "View all employees",
-//         "Add a department",
-//         "Add a role",
-//         "Add an employee",
-//         "Update an employee role",
-//         "EXIT PROGRAM",
-//       ],
-//       name: "actionChosen",
-//     },
-//   ]);
+const Department = require("./models/Department");
+const Employee = require("./models/Employee");
+const Role = require("./models/Role");
 
-//   if (answers.actionChosen === "View all departments") {
-//     db.query("SELECT * FROM department", function (err, results) {
-//       console.log(results);
-//     });
-//   }
-//   if (answers.actionChosen === "View all roles") {
-//     db.query("SELECT * FROM role", function (err, results) {
-//       console.log(results);
-//     });
-//   }
-//   if (answers.actionChosen === "View all employees") {
-//     db.query("SELECT * FROM employee", function (err, results) {
-//       console.log(results);
-//     });
-//   }
-//   if (answers.actionChosen === "Add a department") {
-//     //prompt asking for input on department name
-//     var sql = "INSERT INTO department (name) VALUES (`${}`)";
-//   }
-//   if (answers.actionChosen === "Add a role") {
-//   }
-//   if (answers.actionChosen === "Add an employee") {
-//   }
-//   if (answers.actionChosen === "Update an employee role") {
-//   }
-// }
-// module.exports = startProgram;
+async function startProgram() {
+  const answers = await inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "EXIT PROGRAM",
+      ],
+      name: "actionChosen",
+    },
+  ]);
+
+  if (answers.actionChosen === "View all departments") {
+    try {
+      const tables = await sequelize.getQueryInterface().showAllTables();
+      const table = await sequelize
+        .getQueryInterface()
+        .describeTable(tables[0]);
+      console.log(table);
+      console.log(tables);
+    } catch (err) {
+      console.log(err);
+    }
+    startProgram();
+  }
+  if (answers.actionChosen === "View all roles") {
+    try {
+      const tables = await sequelize.getQueryInterface().showAllTables();
+      const table = await sequelize
+        .getQueryInterface()
+        .describeTable(tables[2]);
+      console.log(table);
+    } catch (err) {
+      console.log(err);
+    }
+    startProgram();
+  }
+  if (answers.actionChosen === "View all employees") {
+    try {
+      const table = await sequelize
+        .getQueryInterface()
+        .describeTable(tables[1]);
+      console.table(table);
+    } catch (err) {
+      console.log(err);
+    }
+    startProgram();
+  }
+  if (answers.actionChosen === "Add a department") {
+    //prompt asking for input on department name
+    const getDepartment = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What department would you like to add?",
+        name: "newDepartment",
+      },
+    ]);
+
+    startProgram();
+  }
+  if (answers.actionChosen === "Add a role") {
+    const getRole = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is the title of the role would you like to add?",
+        name: "roleTitle",
+      },
+      {
+        type: "input",
+        message: "What is the salary of the role?",
+        name: "roleSalary",
+      },
+      {
+        type: "list",
+        message: "What department is the new role in?",
+        choices: [],
+        name: "roleDepartment",
+      },
+    ]);
+  }
+  if (answers.actionChosen === "Add an employee") {
+    const getEmployee = await inquirer.prompt([
+      {
+        type: "input",
+        message:
+          "What is the first name of the employee would you like to add?",
+        name: "employeeFirstName",
+      },
+      {
+        type: "input",
+        message: "What is the last name of the employee would you like to add?",
+        name: "employeeLastName",
+      },
+      {
+        type: "list",
+        message: "What is the employee's role?",
+        choices: [],
+        name: "employeeRole",
+      },
+      {
+        type: "list",
+        message: "Who is the employee's manager?",
+        choices: [],
+        name: "employeeManager",
+      },
+    ]);
+  }
+  if (answers.actionChosen === "Update an employee role") {
+    const updateEmployee = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Which employee's role would you like to update?",
+        choices: [],
+        name: "employeeUpdate",
+      },
+      {
+        type: "list",
+        message: "What role would you like to assign the employee?",
+        choices: [],
+        name: "updatedRole",
+      },
+    ]);
+  }
+  if (answers.actionChosen === "EXIT PROGRAM") {
+    return;
+  }
+}
+
+module.exports = startProgram;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require("./config/connection");
 const Department = require("./models/Department");
 const Role = require("./models/Role");
 const Employee = require("./models/Employee");
-// const startProgram = require("./index.js");
+const startProgram = require("./index.js");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,8 +13,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
-});
-
-// startProgram();
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => console.log("Now listening"));
+  })
+  .then(() => {
+    startProgram();
+  });
