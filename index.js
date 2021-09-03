f1514842964d1a74b2a6f3f529f6bdf5d91bcd1f
Gina Im <eaim928@gmail.com>
2021-09-02 23:11:23 -0600

    add role function working, troubleshooting add employee

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function startProgram() {
       const tables = tableData.map((department) =>
         department.get({ plain: true })
       );
-      // console.log(table);
+      console.log(tables);
       console.table(tables);
     } catch (err) {
       console.log(err);
@@ -77,55 +77,99 @@ async function startProgram() {
     startProgram();
   }
   if (answers.actionChosen === "Add a role") {
-    const getRole = await inquirer.prompt([
-      {
-        type: "input",
-        message: "What is the title of the role would you like to add?",
-        name: "roleTitle",
-      },
-      {
-        type: "input",
-        message: "What is the salary of the role?",
-        name: "roleSalary",
-      },
-      {
-        type: "list",
-        message: "What department is the new role in?",
-        choices: [],
-        name: "roleDepartment",
-      },
-    ]);
-    const insertRole = await Role.findOrCreate({
-      where: { name: getRole.newRole },
-    });
+    try {
+      const tableData = await Department.findAll();
+      let tables = tableData.map((Department) =>
+        Department.get({ plain: true })
+      );
+      console.log(tables);
+      tables = tables.map((each) => {
+        return {
+          name: each.name,
+          value: each.department_id,
+        };
+      });
+      // console.log(table);
+      console.log(tables);
+
+      const getRole = await inquirer.prompt([
+        {
+          type: "input",
+          message: "What is the title of the role would you like to add?",
+          name: "title",
+        },
+        {
+          type: "input",
+          message: "What is the salary of the role?",
+          name: "salary",
+        },
+        {
+          type: "list",
+          message: "What department is the new role in?",
+          choices: tables,
+          name: "department_id",
+        },
+      ]);
+      // console.log(getRole);
+
+      const insertRole = await Role.create(getRole);
+    } catch (err) {
+      console.log(err);
+    }
     startProgram();
   }
+
   if (answers.actionChosen === "Add an employee") {
-    const getEmployee = await inquirer.prompt([
-      {
-        type: "input",
-        message:
-          "What is the first name of the employee would you like to add?",
-        name: "employeeFirstName",
-      },
-      {
-        type: "input",
-        message: "What is the last name of the employee would you like to add?",
-        name: "employeeLastName",
-      },
-      {
-        type: "list",
-        message: "What is the employee's role?",
-        choices: [],
-        name: "employeeRole",
-      },
-      {
-        type: "list",
-        message: "Who is the employee's manager?",
-        choices: [],
-        name: "employeeManager",
-      },
-    ]);
+    try {
+      const tableData = await Role.findAll();
+      let tables = tableData.map((Role) => Role.get({ plain: true }));
+      // console.log(tables);
+      tables = tables.map((each) => {
+        return {
+          name: each.name,
+          value: each.role_id,
+        };
+      });
+      const managerData = await Role.findAll({ title: ["Manager"] });
+      let managers = managerData.map((Role) => Role.get({ plain: true }));
+      console.log(managers);
+      managers = managers.map((each) => {
+        return {
+          name: each.name,
+          value: each.employee_id,
+        };
+      });
+      const getEmployee = await inquirer.prompt([
+        {
+          type: "input",
+          message:
+            "What is the first name of the employee would you like to add?",
+          name: "first_name",
+        },
+        {
+          type: "input",
+          message:
+            "What is the last name of the employee would you like to add?",
+          name: "last_name",
+        },
+        {
+          type: "list",
+          message: "What is the employee's role?",
+          choices: tables,
+          name: "role",
+        },
+        {
+          type: "list",
+          message: "Who is the employee's manager?",
+          choices: managers,
+          name: "employee_id",
+        },
+      ]);
+      const insertEmployee = await Role.create(getEmployee);
+    } catch (err) {
+      console.log(err);
+    }
+    startProgram();
   }
   if (answers.actionChosen === "Update an employee role") {
     const updateEmployee = await inquirer.prompt([
